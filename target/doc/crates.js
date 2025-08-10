6d30a9d19b8e5632772494f94610c223a32ea986
Anglebase <1628818324@qq.com>
2025-08-10 11:41:07 +0800

    移除文档

--- target/doc/crates.js
+++
@@ -1,2 +0,0 @@
-window.ALL_CRATES = ["xege"];
-//{"start":21,"fragment_lengths":[6]}
--- target/doc/trait.impl/core/ops/deref/trait.Deref.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["xege",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"xege/color/struct.Color.html\" title=\"struct xege::color::Color\">Color</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[271]}
